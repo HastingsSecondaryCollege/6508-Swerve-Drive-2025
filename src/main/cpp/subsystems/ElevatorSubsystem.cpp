4516cb3f1a380b8ae319b7a11c864bdd6e32e57d
ryfi94 <isaac.lavis@education.nsw.gov.au>
2025-03-24 12:57:56 +1100

    Revert "changes after crash"
    This reverts commit b772bdc3bb458345b42f653b096ea48323ab3acb.

--- src/main/cpp/subsystems/ElevatorSubsystem.cpp
+++ src/main/cpp/subsystems/ElevatorSubsystem.cpp
@@ -28,16 +28,16 @@ ElevatorSubsystem::ElevatorSubsystem()
   leadElevatorMotorConfig.Slot0.kP = 10.0;
   //leadElevatorMotorConfig.Slot0.kA = 0.1;
   //leadElevatorMotorConfig.Slot0.kV = 0.12;
-  leadElevatorMotorConfig.MotionMagic.MotionMagicAcceleration = 300.0_rad_per_s_sq;
-  leadElevatorMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 300.0_rad_per_s;
-  leadElevatorMotorConfig.MotionMagic.MotionMagicJerk = 3000_rad_per_s_cu;
+  leadElevatorMotorConfig.MotionMagic.MotionMagicAcceleration = 200.0_rad_per_s_sq;
+  leadElevatorMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 200.0_rad_per_s;
+  leadElevatorMotorConfig.MotionMagic.MotionMagicJerk = 2000_rad_per_s_cu;
 
   wristMotorConfig.Slot0.kP = 10.0;
    wristMotorConfig.Slot0.kA = 0.1;
    wristMotorConfig.Slot0.kV = 0.12;
-  wristMotorConfig.MotionMagic.MotionMagicAcceleration = 40.0_rad_per_s_sq;
-  wristMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 40.0_rad_per_s;
-  wristMotorConfig.MotionMagic.MotionMagicJerk = 400_rad_per_s_cu;
+  wristMotorConfig.MotionMagic.MotionMagicAcceleration = 20.0_rad_per_s_sq;
+  wristMotorConfig.MotionMagic.MotionMagicCruiseVelocity = 20.0_rad_per_s;
+  wristMotorConfig.MotionMagic.MotionMagicJerk = 200_rad_per_s_cu;
 
   coralMotorConfig.Slot0.kS = 0.1;  // To account for friction, add 0.1 V of static feedforward
   coralMotorConfig.Slot0.kV = 0.12; // Kraken X60 is a 500 kV motor, 500 rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / rotation per second
